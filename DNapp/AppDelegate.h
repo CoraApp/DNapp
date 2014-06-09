f63fa58dc781ae57e92e54274020c8ac11102e4f
Paul Martens <paul@cora.io>
2014-06-09 11:44:42 -0400

    First Commit
    Testing Design & Code

---
+++ DNapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DNapp
+//
+//  Created by Paul Martens on 6/4/14.
+//  Copyright (c) 2014 Paul Martens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DNapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DNapp
+//
+//  Created by Paul Martens on 6/4/14.
+//  Copyright (c) 2014 Paul Martens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
